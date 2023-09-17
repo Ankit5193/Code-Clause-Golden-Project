c45067028d05d207b27c3a320f4b9e68cdeb3eb1
Ankit Yadav <115175587+Ankit5193@users.noreply.github.com>
2023-09-17 11:58:02 +0530

    Add files via upload

---
+++ javascript/music-list.js
@@ -0,0 +1,32 @@
+let allMusic = [
+  {
+    name: "Mi Amor",
+    artist: "Sharn ",
+    img: "song-1",
+    src: "song-1",
+  },
+  {
+    name: "Under The Influence",
+    artist: "Chris Brown",
+    img: "song-2",
+    src: "song-2",
+  },
+  {
+    name: "Spaceship",
+    artist: "Ap Dhillon",
+    img: "song-3",
+    src: "song-3",
+  },
+  {
+    name: "StarBoy",
+    artist: "The Weekend, Daft Punk",
+    img: "song-4",
+    src: "song-4",
+  },
+  {
+    name: "Mahiye Jinna Sohna",
+    artist: "Darshan Raval",
+    img: "song-5",
+    src: "song-5",
+  },
+];
